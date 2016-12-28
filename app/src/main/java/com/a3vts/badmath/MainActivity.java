095592488c855f1c9974699e41d2375ac5cdc480
Santiesteban <alvaro.alfaro@outlook.com>
2016-12-28 16:49:43 -0600

    Initial commit

---
+++ app/src/main/java/com/a3vts/badmath/MainActivity.java
@@ -0,0 +1,78 @@
+package com.a3vts.badmath;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtPercentage;
+    EditText txtNumber;
+    TextView lblTotal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        txtPercentage = (EditText) findViewById(R.id.txtPercentage);
+        txtNumber = (EditText) findViewById(R.id.txtNumber);
+        lblTotal = (TextView) findViewById(R.id.lblTotal);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        Button cmdCalculate = (Button) findViewById(R.id.cmdCalculate);
+        cmdCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float total = 0;
+                float percentage = 0;
+                float number = 0;
+                percentage = Float.parseFloat(txtPercentage.getText().toString());
+                number = Float.parseFloat(txtNumber.getText().toString());
+                total = number * (percentage/100);
+                lblTotal.setText(Float.toString(total));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
